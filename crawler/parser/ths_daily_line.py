3b43406ec472c841df1c1736eae51cc833be2469
exiaohao <exiaohao@gmail.com>
2016-11-17 23:51:25 +0800

    add structure

---
+++ crawler/parser/ths_daily_line.py
@@ -0,0 +1,12 @@
+__author__ = 'exiaohao@gmail.com'
+__data_source__ = 'http://d.10jqka.com.cn/v2/line/hs_{stock_id}/01/last.js'
+__request_method__ = 'GET'
+__headers__ = {
+    "User-Agent": "Chrome"
+}
+
+
+class Parser:
+    def __init__(self, data):
+        pass
+
