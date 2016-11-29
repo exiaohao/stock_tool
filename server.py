0f86c63a229aae8646cb05cee0959abd723450f2
exiaohao <songhao@cmee.zjxu.edu.cn>
2016-11-29 17:15:08 +0800

    add demo

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
+from pymongo import MongoClient
 from crawler.libs.request_ths import THSRequestLib
 from crawler.exceptions import RequestException
 
 ths = THSRequestLib(THSRequestLib.REQU_DAILY_LINE)
 
-try:
-    ths.data('010976')
-except RequestException as ex:
-    pass
+client = MongoClient('127.0.0.1', 17017)
+db = client['stock']
+daily = db['daily']
+
+for stock_id in range(600000, 6039999):
+    try:
+        result = ths.data(str(stock_id))
+        daily.insert({'data':result, 'stock_id': stock_id})
+    except RequestException as ex:
+        print(ex)
