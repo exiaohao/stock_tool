f4bd08ef728b5679cc73a7fdaf2a1056cd11bf23
exiaohao <songhao@cmee.zjxu.edu.cn>
2016-12-01 11:03:23 +0800

    add data_source

--- crawler/parser/ths_today_line.py
+++ crawler/parser/ths_today_line.py
@@ -1,1 +1,2 @@
-__data_source__ = 'http://d.10jqka.com.cn/v2/line/hs_000976/01/today.js'
+# __data_source__ = 'http://d.10jqka.com.cn/v2/line/hs_000976/01/today.js'
+__data_source__ = 'http://d.10jqka.com.cn/v2/moneyflow/hs_{stock_id}/last.js'
